8c71b84e71f941e608313be4fd7aa2660080d098
Elita App Deploy <elita-app@local>
2026-08-05 00:41:42 +0000

    Debug: mostra diagnóstico do sincronismo de marmita na própria tela (alerta), pra funcionar tambem no celular

---
+++ assets/chevron-left-C7zyYeVj.js
@@ -0,0 +1,1 @@
+import{p as e}from"./index-DZecr6t_.js";var t=e(`chevron-left`,[[`path`,{d:`m15 18-6-6 6-6`,key:`1wnfg3`}]]);export{t};
--- assets/chevron-right-CxH6VK52.js
+++
@@ -1,1 +0,0 @@
-import{p as e}from"./index-B3j0b5np.js";var t=e(`chevron-right`,[[`path`,{d:`m9 18 6-6-6-6`,key:`mthhwq`}]]);export{t};
