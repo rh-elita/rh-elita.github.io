5e42d809c21e649b22327d088003ed0043078f7e
Elita App Deploy <elita-app@local>
2026-08-05 00:58:11 +0000

    Debug: mostra versao/contagem de planos direto na tela de cada dia

---
+++ assets/chevron-left-CFYA1U0i.js
@@ -0,0 +1,1 @@
+import{p as e}from"./index-CMTdttk8.js";var t=e(`chevron-left`,[[`path`,{d:`m15 18-6-6 6-6`,key:`1wnfg3`}]]);export{t};
--- assets/chevron-right-D_AXM7Ro.js
+++
@@ -1,1 +0,0 @@
-import{p as e}from"./index-CkBG4E3G.js";var t=e(`chevron-right`,[[`path`,{d:`m9 18 6-6-6-6`,key:`mthhwq`}]]);export{t};
